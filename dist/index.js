95c305785f1dea9c515023b93cb7ba3425f0dd8c
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-11-12 03:47:45 -0500

    publishing compiled form
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = null;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxrQkFBZSxJQUFJLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBudWxsXG4iXX0=
