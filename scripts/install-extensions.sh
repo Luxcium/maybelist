4752beff1c9645336d95f2d6d1c9a3bac2f3f97c
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-11-12 03:34:24 -0500

    Scientia es lux principium✨

---
+++ scripts/install-extensions.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Installs recomended extensions
+
+code --install-extension aaron-bond.better-comments
+code --install-extension CoenraadS.bracket-pair-colorizer-2
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension ghmcadams.lintlens
+code --install-extension Luxcium.pop-n-lock-theme-vscode
+code --install-extension ms-vscode.typescript-javascript-grammar
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension PKief.material-icon-theme
+code --install-extension SophodorosLLC.codevalet-vscode
+code --install-extension usernamehw.errorlens
+
+exit $?
