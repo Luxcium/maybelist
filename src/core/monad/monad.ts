e65bc96c8ee626135a319038d71f66a0d37ddf2a
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-11-24 15:12:42 -0500

    To Show To My Friend Paolo DiBello doble Zero
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/core/monad/monad.ts
+++ src/core/monad/monad.ts
@@ -7,37 +7,31 @@ interface IMonad<CVal> {
   ap<R>(functor: Functor<FnAtoB<CVal, R>>): Monad<R>;
   chain<R>(fn: FnAtoB<CVal, Monad<R>>): Monad<R>;
 }
-
 class Monad<MVal> extends Chain<MVal> implements IMonad<MVal> {
   public static of<TVal>(value: TVal): Monad<TVal> {
     return new Monad<TVal>(value);
   }
-
   public static fromValueOf<TVal>(value: Functor<TVal>): Monad<TVal> {
     return Monad.of<TVal>(JSON.parse(JSON.stringify(value.fork)));
   }
-
   public constructor(value: MVal) {
     super(value);
     this['fantasy-land/map'] = this.map;
     this['fantasy-land/ap'] = this.ap;
     this['fantasy-land/chain'] = this.chain;
   }
-
   public declare ['fantasy-land/map'];
   public map<R>(fn: (val: MVal) => R): Monad<R> {
     return Monad.of<R>(
       super.map<R>(x => fn(x)).fork,
     );
   }
-
   public declare ['fantasy-land/ap'];
   public ap<R>(functor: Functor<FnAtoB<MVal, R>>): Monad<R> {
     return functor.map<Monad<R>>((fn: (val: MVal) => R) =>
       this.map<R>(x => fn(x)),
     ).fork;
   }
-
   public declare ['fantasy-land/chain'];
   public chain<R>(fn: FnAtoB<MVal, Monad<R>>): Monad<R> {
     return Monad.of<Monad<R>>(
@@ -45,5 +39,4 @@ class Monad<MVal> extends Chain<MVal> implements IMonad<MVal> {
     ).fork;
   }
 }
-
 export { Monad };
