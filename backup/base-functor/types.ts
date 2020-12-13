9ab2b35d152ee34d51d4aba147505a1098e3476b
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-12-13 12:46:17 -0500

    cmented out old arcived deprecated code
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- backup/base-functor/types.ts
+++ backup/base-functor/types.ts
@@ -1,10 +1,11 @@
-import { FnAtoB } from '../../src';
-import { IFMap } from '../types';
+// import { FnAtoB } from '../../src';
+// import { IFMap } from '../types';
 
-/** @deprecated */
-interface IBaseFunctor<A = unknown> extends IFMap<A> {
-  fMap<B = unknown>(fn: FnAtoB<A, B>): IBaseFunctor<B>;
-  ['fantasy-land/map']: any;
-}
+// /** @deprecated */
+// interface IBaseFunctor<A = unknown> extends IFMap<A> {
+//   fMap<B = unknown>(fn: FnAtoB<A, B>): IBaseFunctor<B>;
+//   ['fantasy-land/map']: any;
+// }
 
-export type { IBaseFunctor };
+// export type { IBaseFunctor };
+// //
