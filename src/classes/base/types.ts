bf9c3dbf7227309fa315d5bdcc6e8a50b4c3af12
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-12-13 12:50:55 -0500

    extends AbstractBase subtypes definition
    build & test: 8.9152s
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/classes/base/types.ts
+++ src/classes/base/types.ts
@@ -5,13 +5,21 @@ interface IClone<Ax = unknown> {
   readonly clone: Ax;
 }
 
+interface ToString {
+  toString(): string;
+}
+
+interface ValueOf<Ax = unknown> {
+  valueOf(): Ax;
+}
+
 type ForkType<Ax = unknown> = IFork<Ax> & IClone<Ax>;
 
-interface AbstractBase<A> extends IFork<A>, IClone<A> {
+interface AbstractBase<A> extends IFork<A>, IClone<A>, ToString, ValueOf<A> {
   readonly fork: A;
   readonly clone: A;
   toString(): string;
   valueOf(): A;
 }
 
-export type { AbstractBase, ForkType, IClone, IFork };
+export type { AbstractBase, ForkType, IClone, IFork, ToString, ValueOf };
