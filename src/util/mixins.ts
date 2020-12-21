88e8b17023ab04aa60605ded1d7857f6c90bfbc3
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-12-21 18:31:47 -0500

    use mixin from @basarat 🌹without autorisation
    !! NOTICE !!
    
    https://gist.github.com/basarat/61860d3b035ead022af3dff67df91a8b
    
    https://www.youtube.com/watch?v=JNmqxEcJjr4
    
    https://creativecommons.org/licenses/by/4.0/
    (🌹INTERIM_LICENSE🌹)
    
    © 2020 Basarat Ali * Used without autorisation from the author(s)
    
    This part of the code may be removed in future releases
    it will be your responsability to make sure this part of code
    is still usable under the interim licence CC BY 4.0 in the future...
    
    I will remove the comment from the source code after
    I get the permission to use the code from the copyright holder(s)
    under CC BY 4.0 or under MIT...
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

---
+++ src/util/mixins.ts
@@ -0,0 +1,45 @@
+// !! NOTICE !!
+// https://gist.github.com/basarat/61860d3b035ead022af3dff67df91a8b
+// https://www.youtube.com/watch?v=JNmqxEcJjr4
+// https://creativecommons.org/licenses/by/4.0/ (🌹INTERIM_LICENSE🌹)
+//+ © 2020 Basarat Ali * Used without autorisation from the author(s) this part
+//+ of the code may be removed in future releases it will be your responsability
+//+ to make sure this part of code is still usable under
+//+ the interim licence CC BY 4.0 in the future... I will remove this comment
+//+ after I get the copyright holder(s) the permission to use the
+//+ code under CC BY 4.0 or under MIT...
+
+type Class = new (...args: any[]) => any;
+
+function DisposableMixin<Base extends Class>(base: Base) {
+  return class extends base {
+    isDisposed: boolean = false;
+    dispose() {
+      this.isDisposed = true;
+    }
+  };
+}
+
+function ActivatableMixin<Base extends Class>(base: Base) {
+  return class extends base {
+    isActive: boolean = false;
+    activate() {
+      this.isActive = true;
+    }
+    deactivate() {
+      this.isActive = false;
+    }
+  };
+}
+
+class Example extends DisposableMixin(ActivatableMixin(class {})) {
+  member = 123;
+  constructor() {
+    super();
+  }
+}
+const example: Example = new Example();
+void example;
+
+export { ActivatableMixin, DisposableMixin };
+export type { Class };
