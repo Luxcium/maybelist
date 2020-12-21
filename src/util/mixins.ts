eea2e76b5ce36f938b7d4f615441e2c501a7f328
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2020-12-21 18:44:34 -0500

    use mixin from @basarat 🌹 without autorisation
    !! NOTICE !!
    
    https://gist.github.com/basarat/61860d3b035ead022af3dff67df91a8b
    
    https://www.youtube.com/watch?v=JNmqxEcJjr4
    
    https://creativecommons.org/licenses/by/4.0/ (🌹INTERIM_LICENSE🌹)
    
    © 2020 Basarat Ali * Used without authorization from the author(s)
    
    This part of the code may be removed in future releases
    it will be your responsibility to make sure this part of the code
    is still usable under the interim license CC BY 4.0 in the future...
    
    I will remove the comment from the source code after
    I get permission to use the code from the copyright holder(s)
    under CC BY 4.0 or under MIT...
    
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/util/mixins.ts
+++ src/util/mixins.ts
@@ -2,10 +2,17 @@
 // https://gist.github.com/basarat/61860d3b035ead022af3dff67df91a8b
 // https://www.youtube.com/watch?v=JNmqxEcJjr4
 // https://creativecommons.org/licenses/by/4.0/ (🌹INTERIM_LICENSE🌹)
-//+ © 2020 Basarat Ali * Used without autorisation from the author(s) this part
-//+ of the code may be removed in future releases it will be your responsability
+//+ © 2020 Basarat Ali * Used without authorization from the author(s) this part
+//+ of the code may be removed in future releases it will be your responsibility
 //+ to make sure this part of code is still usable under
-//+ the interim licence CC BY 4.0 in the future... I will remove this comment
+//+ the interim license CC BY 4.0 in the future... I will remove this comment
+//+ after I get the copyright holder(s) the permission to use the
+//+ code under CC BY 4.0 or under MIT...
+
+//+ © 2020 Basarat Ali * Used without authorization from the author(s) this part
+//+ of the code may be removed in future releases it will be your responsibility
+//+ to make sure this part of code is still usable under
+//+ the interim license CC BY 4.0 in the future... I will remove this comment
 //+ after I get the copyright holder(s) the permission to use the
 //+ code under CC BY 4.0 or under MIT...
 
